1b536f8611fce00c2d63ed16a5fc2d513ce23c10
sopanha09 <sopanhasous@gmail.com>
2024-02-16 14:13:03 +0700

    add apiSlice to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { todoApi } from "../api/todoApi";
 
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    [todoApi.reducerPath]: todoApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todoApi.middleware),
 });
