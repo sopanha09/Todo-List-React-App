ead16d2ce959e7b76461c13f075c0a349c34949a
sopanha09 <sopanhasous@gmail.com>
2024-02-19 18:04:23 +0700

    custom style...

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TodoList from "./components/TodoList";
 const App = () => {
   return (
     <div className="app">
+      <header>
+        <h2>Today's Task</h2>
+      </header>
       <TodoList />
     </div>
   );
