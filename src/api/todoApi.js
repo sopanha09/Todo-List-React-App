828083faeb802c07e6c19b2066694b037c0290c5
sopanha09 <118888581+sopanha09@users.noreply.github.com>
2024-02-21 10:31:35 +0700

    resolve failed deploy.

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+
 export const todoApi = createApi({
   reducerPath: "todoApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/" }),
